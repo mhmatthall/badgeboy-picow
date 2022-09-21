d69fbd4908f0064a9c6e45befa61ff8e51b913d1
Matt <42594962+mhmatthall@users.noreply.github.com>
2022-09-21 05:04:16 +0100

    Debug and tidy logic, test working with single badge

--- src/main.py
+++ src/main.py
@@ -9,21 +9,56 @@ This requires the MicroPython binaries to be loaded onto the Pico already.
 by Matt Hall
 
 """
+from machine import Pin, Timer
 import network
 import time
 import rp2
+import ujson
 import ubinascii
 import urequests as req
 
 WLAN_SSID = 'Badge City'
-WLAN_PW = 'ilovecomputers'
-WLAN_SUBNET = '192.168.0'
-WLAN_SERVER_IP = '2'
-WLAN_SERVER_URL = WLAN_SUBNET + WLAN_SERVER_IP
+WLAN_PW = 'ihatecomputers'
+WLAN_SUBNET = '192.168.1'
+WLAN_SERVER_IP = '3'
+WLAN_SERVER_PORT = '3001'
+WLAN_SERVER_URL = WLAN_SUBNET + '.' + WLAN_SERVER_IP + ':' + WLAN_SERVER_PORT
 
-REQUEST_HEADER = '{ "Content-Type": "application/json" }'
+REQUEST_HEADER = { "Content-Type": "application/json" }
 
 BADGE_DATA_CACHE = ''
+
+# Control onboard LED as a status indicator
+led = Pin('LED', Pin.OUT)
+led_timer = Timer()
+
+def blink_led(_timer):
+    global led
+    led.toggle()
+
+def connect_to_wifi():
+    print('* Connecting to WLAN...')
+
+    # Try to establish connection
+    wlan.connect(WLAN_SSID, WLAN_PW)
+
+    # We don't want to ever stop trying to connect for resiliency
+    while wlan.status() != 3:
+        print('  ...' + str(wlan.status()))
+        time.sleep(1)
+
+    # Log local IP address
+    print(f'    Connected to \'{WLAN_SSID}\' with address {wlan.ifconfig()[0]}')
+
+
+def display_boot_screen():
+    print('* Hello world!\n    Clearing the display...')
+    
+    # Blink LED at 10Hz in init stage
+    led_timer.init(freq=10, mode=Timer.PERIODIC, callback=blink_led)
+
+    # Clear the display and show a loading thing
+    # TODO
 
 # Display a boot screen to indicate device powered on
 display_boot_screen()
@@ -40,35 +75,38 @@ wlan.active(True)
 # wlan.config(pm=0xa11140)
 
 # Get our MAC address
-MAC = upper(ubinascii.hexlify(
-        network.WLAN().config('mac')).decode()
-        )
+MAC = ubinascii.hexlify(
+    network.WLAN().config('mac')
+).decode().upper()
 
 # Log MAC
-print(f"This device's MAC: {MAC}")
+print(f"* This device's MAC address is {MAC}")
 
 # Connect to network (WARNING: will infinite loop until connected)
 connect_to_wifi()
 
 # Main event loop
 while True:
+    # Blink LED at 2Hz in main event loop
+    led_timer.init(freq=2, mode=Timer.PERIODIC, callback=blink_led)
+
     try:
-        print('Polling API...')
+        print('* Polling API...')
 
         # Poll DB for changes every X seconds
         poll_request = req.get(
-            f'{WLAN_SERVER_URL}/api/badges/{MAC}',
+            f'http://{WLAN_SERVER_URL}/api/badges/{MAC}',
             headers=REQUEST_HEADER
         )
 
         # If found in DB
         if poll_request.status_code == 200:
-            print('Found badge!')
+            print('    Badge exists in DB')
             badge_data = poll_request.json()
 
             # Only change things when the server data has changed
             if badge_data != BADGE_DATA_CACHE:
-                print('Badge data cache out of date. Refreshing...')
+                print('    Badge data cache out of date. Refreshing...')
 
                 # Update data cache
                 BADGE_DATA_CACHE = badge_data
@@ -78,64 +116,47 @@ while True:
 
         # If not found in DB
         elif poll_request.status_code == 404:
-            print('Badge not found!\nInserting blank DB record...')
+            print('    Badge not found!\n      Inserting blank DB record...')
 
             # Insert blank DB record
             create_badge_request = req.post(
-                f'{WLAN_SERVER_URL}/api/badges/{MAC}',
+                f'http://{WLAN_SERVER_URL}/api/badges',
                 headers=REQUEST_HEADER,
-                data='{ "macAddress":"' + MAC + '", "name":"", "pronouns":"", "affiliation":"", "message":"", "image":"" }'
+                data=ujson.dumps(
+                    { "macAddress":MAC, "name":"", "pronouns":"", "affiliation":"", "message":"", "image":"" }
+                )
             )
 
             # If successful
             if create_badge_request.status_code == 201:
-                print(f'Successfully created new DB record for badge {MAC}')
+                print(f'      Successfully created new DB record for badge {MAC}')
 
                 # Display instructions
                 # TODO
 
             else:
-                print(f'ERROR: Could not create new badge record in DB. API returned status {create_badge_request.status_code}')
+                print(f'      ERROR: Could not create new badge record in DB. API returned status {create_badge_request.status_code}')
             
             # Finally, close socket
             create_badge_request.close()
 
         else:
-            print(f'ERROR: DB poll failed. API returned status {poll_request.status_code}')
+            print(f'    ERROR: DB poll failed. API returned status {poll_request.status_code}')
+            
+        # Clean up connection
+        poll_request.close()
     
-    except err:
-        print(f'ERROR: Could not complete network request:\n{err}')
+    except Exception as err:
+        print(f'    ERROR: Could not complete network request:\n    {err}')
+        
+        # Blink LED at 5Hz when errored
+        led_timer.init(freq=5, mode=Timer.PERIODIC, callback=blink_led)
 
         #  Check if network was to blame
         if wlan.status() < 0 or wlan.status() > 3:
-            print('REASON: Network connection was lost. Attempting reconnect...')
+            print('      REASON: Network connection was lost. Attempting reconnect...')
             wlan.disconnect()
             connect_to_wifi()   # WARNING: will continue forever until reconnected
-    
-    finally:
-        poll_request.close()
 
-    print('Event loop complete. Sleeping...')
+    print('* Event loop complete. Sleeping...')
     time.sleep(20)
-
-
-def connect_to_wifi():
-    print('Connecting to WLAN...')
-
-    # Try to establish connection
-    wlan.connect(WLAN_SSID, WLAN_PW)
-
-    # We don't want to ever stop trying to connect for resiliency
-    while not wlan.connected() and wlan.status() == 0:
-        print('  ...')
-        time.sleep(1)
-
-    # Log local IP address
-    print(f'  Connected with IP {wlan.ifconfig()[0]}')
-
-
-def display_boot_screen():
-    print('Hello world!\nClearing the display...')
-
-    # Clear the display and show a loading thing
-    # TODO
