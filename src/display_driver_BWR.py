5ff91b12f06963ccca7cb680105909548483a806
Matt <42594962+mhmatthall@users.noreply.github.com>
2023-01-15 03:55:22 +0000

    Optimise driver file size and hardware performance

--- src/display_driver_BWR.py
+++ src/display_driver_BWR.py
@@ -1,6 +1,6 @@
 """ Pico-ePaper-2.9-B display driver
         by: Matt Hall
-        version: 0.1
+        version: 0.2
 
     Adapted from 'Pico_ePaper_Code' (https://github.com/waveshare/Pico_ePaper_Code)
         by: Waveshare team
@@ -8,8 +8,7 @@
         license: MIT
 """
 from machine import Pin, SPI
-import framebuf
-import utime
+from utime import sleep
 
 # Toggle print debugging
 DEBUG = False
@@ -19,7 +18,7 @@ DISPLAY_WIDTH = 128
 DISPLAY_HEIGHT = 296
 
 # Pinout
-DC_PIN = 8      # Data/Command pin
+DC_PIN = 8      # Data/Command pin (0=cmd, 1=data)
 CS_PIN = 9      # Chip Select pin
 # pin 10 is the CLK
 # pin 11 is the MOSI
@@ -32,154 +31,113 @@ Driver class for the Waveshare 2.9" ePaper display for Pico (pico-e-paper-2.9-b)
 class DisplayDriver:
     def __init__(self):
         if DEBUG: print('* Initialising display module interface...')
+        # Init pin layout
+        self.__dc_pin = Pin(DC_PIN, Pin.OUT)
+        self.__cs_pin = Pin(CS_PIN, Pin.OUT)
         self.__reset_pin = Pin(RESET_PIN, Pin.OUT)
         self.__busy_pin = Pin(BUSY_PIN, Pin.IN, Pin.PULL_UP)
-        self.__cs_pin = Pin(CS_PIN, Pin.OUT)
+
+        # Init SPI connection
+        self.__spi = SPI(1, baudrate=4_000_000)
 
         self.width = DISPLAY_WIDTH
         self.height = DISPLAY_HEIGHT
 
-        self.__spi = SPI(1, baudrate=4000000)
-        self.__dc_pin = Pin(DC_PIN, Pin.OUT)
-
-        # Create a buffer to store the display data
-        self.__buf = bytearray(self.height * self.width // 8)
-
-        self.__image_buffer = framebuf.FrameBuffer(
-            self.__buf, self.width, self.height, framebuf.MONO_HLSB
-        )
-
         if DEBUG: print('* Initialising display...')
 
-        # Hardware reset
+        # Reset and send power on cmd
         self.__hw_reset()
-        self.__wait_for_display()
-
-        # Send power on cmd
         self.__send_command(0x04)
         self.__wait_for_display()
 
-        # PANEL SETTING REGISTER (PSR) ---
+        # ------------------------------------------------------------
+        # PANEL SETTING REGISTER (PSR)
         # Send panel setting cmd
         self.__send_command(0x00)
 
-        # 1 - set resolution pt1
+        # 0 - set resolution pt1
         # 0 - set resolution pt2
         # 0 - get lut from otp (DON'T SPECIFY)
-        # 1 - black/white only mode ENABLE
+        # 0 - black/white only mode DISABLE
         # 1 - gate scan direction UP
         # 1 - source shift direction RIGHT
         # 1 - booster ON
         # 1 - soft reset DOES NOTHING
-        self.__send_data(0x9f)
+        self.__send_data(0x0f)
+        
+        # ------------------------------------------------------------
+        # RESOLUTION SETTING (TRES)
+        # (OVERRIDES PSR RESOLUTION OPTIONS)
+        self.__send_command(0x61)
+        self.__send_data(0x80)
+        self.__send_data(0x01)
+        self.__send_data(0x28)
 
-        # VCOM AND DATA INTERVAL REGISTER ---
-        # Send CDI command
+        # ------------------------------------------------------------
+        # VCOM AND DATA INTERVAL REGISTER (CDI)
+        # Send config
         self.__send_command(0x50)
 
         # 0 - VBD pt1
-        # 0 - VBD pt2
+        # 1 - VBD pt2
         # 1 - DDX pt1
-        # 0 - DDX pt2
+        # 1 - DDX pt2
         # 0 - default data interval of 10 frames (base-10)
         # 1 - default data interval of 10 frames (base-10)
         # 1 - default data interval of 10 frames (base-10)
         # 1 - default data interval of 10 frames (base-10)
-        self.__send_data(0x27)
-
-    def __digital_write(self, pin, value):
-        pin.value(value)
-
-    def __digital_read(self, pin):
-        return pin.value()
+        self.__send_data(0x77)
 
     def __delay_ms(self, delaytime):
-        utime.sleep(delaytime / 1000.0)
-
-    def __spi_writebyte(self, data):
-        self.__spi.write(bytearray(data))
-
-    def __spi_read(self, num_bytes):
-        return bytearray(self.__spi.read(num_bytes))
-
-    def __module_exit(self):
-        self.__digital_write(self.__reset_pin, 0)
+        sleep(delaytime / 1000.0)
 
     def __hw_reset(self):
-        self.__digital_write(self.__reset_pin, 1)
+        self.__reset_pin.value(1)
         self.__delay_ms(50)
-        self.__digital_write(self.__reset_pin, 0)
+
+        self.__reset_pin.value(0)
         self.__delay_ms(2)
-        self.__digital_write(self.__reset_pin, 1)
+
+        self.__reset_pin.value(1)
         self.__delay_ms(50)
 
     def __send_command(self, command):
-        self.__digital_write(self.__dc_pin, 0)
-        self.__digital_write(self.__cs_pin, 0)
-        self.__spi_writebyte([command])
-        self.__digital_write(self.__cs_pin, 1)
+        # Command mode
+        self.__dc_pin.value(0)
+
+        # Slave chip select and send command, then return to master
+        self.__cs_pin.value(0)
+        self.__spi.write(bytearray([command]))
+        self.__cs_pin.value(1)
 
     def __send_data(self, data):
-        self.__digital_write(self.__dc_pin, 1)
-        self.__digital_write(self.__cs_pin, 0)
-        self.__spi_writebyte([data])
-        self.__digital_write(self.__cs_pin, 1)
+        # Data mode
+        self.__dc_pin.value(1)
 
-    def __receive_data(self, num_bytes):
-        self.__digital_write(self.__dc_pin, 1)
-        self.__digital_write(self.__cs_pin, 0)
-        rx = self.__spi_read(num_bytes)
-        self.__digital_write(self.__cs_pin, 1)
-        return rx
+        # Slave chip select and send command, then return to master
+        self.__cs_pin.value(0)
+        self.__spi.write(bytearray([data]))
+        self.__cs_pin.value(1)
 
     def __wait_for_display(self):
         if DEBUG: print('    Rendering...')
-
-        # Set upper bounds of 120 seconds for display to update
-        for i in range(120):
-            if self.__digital_read(self.__busy_pin) == 1: break
+        # Set upper bounds of 30 seconds for display to update; check every 1 second
+        for i in range(30):
+            # Rendering takes time so we monitor the BUSY pin that signals when
+            # the microcontroller has finished the render (0=busy, 1=free)
+            if self.__busy_pin.value() == 1: break
             self.__delay_ms(1000)
 
         if DEBUG: print('    Rendering complete.')
 
     def __refresh_display(self):
-        # Send DRF cmd
+        # Send display refresh cmd (DRF)
         self.__send_command(0x12)
         self.__wait_for_display()
 
-    def __display_buffer_img(self):
-        # Start tx 2 to SRAM (DTM2)
-        self.__send_command(0x13)
-
-        # For each row (0-295)
-        for j in range(self.height):
-            # For each col / 8 (0-15)
-            for i in range(int(self.width / 8)):
-                # Send byte to display, where each bit represents a pixel
-                self.__send_data(self.__buf[i + j * int(self.width / 8)])
-
-        self.__refresh_display()
-
-    def __debug_display_lines(self):
-        # Start tx 2 to SRAM (DTM2)
-        self.__send_command(0x13)
-
-        # For each line in the long side
-        for j in range(self.height):
-            # Send in (128 / 8 =) 16B chunks
-            for i in range(int(self.width / 8)):
-                if (j % 4) == 0:
-                    if DEBUG: print(f'    setting {0x00} at ({i}, {j})')
-                    self.__send_data(0x00)
-                else:
-                    if DEBUG: print(f'    setting {0xff} at ({i}, {j})')
-                    self.__send_data(0xff)
-
-        self.__refresh_display()
-
-    def __clear_display(self, val_to_write=0xff):
-        # Start tx 2 to SRAM (DTM2)
-        self.__send_command(0x13)
+    def __fill_display(self, val_to_write=0xff):
+        # Start B+W pixel data tx to SRAM (DTM1)
+        self.__send_command(0x10)
 
         for j in range(self.height):
             for i in range(int(self.width / 8)):
@@ -197,14 +155,33 @@ class DisplayDriver:
         self.__send_command(0x07)
         self.__send_data(0xA5)
 
+    def debug_display_stripes(self):
+        if DEBUG: print("* DEBUG CMD: STRIPES")
+
+        # Start B+W pixel data tx to SRAM (DTM1)
+        self.__send_command(0x10)
+
+        # For each line in the long side
+        for j in range(self.height):
+            # Send in (128 / 8 =) 16B chunks
+            for i in range(int(self.width / 8)):
+                if (j % 4) == 0:
+                    if DEBUG: print(f'    setting {0x00} at ({i}, {j})')
+                    self.__send_data(0x00)
+                else:
+                    if DEBUG: print(f'    setting {0xff} at ({i}, {j})')
+                    self.__send_data(0xff)
+
+        self.__refresh_display()
+
     def display(self, image):
         """ Push an image to the display module and display it. Images are expected to be contiguous
         hex strings, where each pair of hex values represents 8 pixels to display.
 
         e.g., '0e' corresponds to the 8 pixel segment: '00001110'
         """
-        # Start tx 2 to SRAM (DTM2)
-        self.__send_command(0x13)
+        # Start B+W pixel data tx to SRAM (DTM1)
+        self.__send_command(0x10)
 
         if DEBUG: print('* Starting render...')
 
